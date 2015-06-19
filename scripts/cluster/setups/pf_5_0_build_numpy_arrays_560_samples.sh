459322f1cb63dcecb5c5d81fa32e26c7a97532f4
Richard Pearson <rpearson@well.ox.ac.uk>
2015-06-19 10:40:00 +0100

    work on pgv5.0 filtering

--- scripts/cluster/setups/pf_5_0_build_numpy_arrays_560_samples.sh
+++ scripts/cluster/setups/pf_5_0_build_numpy_arrays_560_samples.sh
@@ -2,7 +2,7 @@
 # ./scripts/cluster/setups/pf_5_0_build_numpy_arrays_560_samples.sh
 
 export SETUPNAME=5_0
-export VCF_FN=/data/mirror/nfs/team112_internal/production_files/Pv/5_0/pf_50_vfp1.vcf
+export VCF_FN=/data/mirror/nfs/team112_internal/production_files/Pv/5_0/pf_50_vfp1.vcf.gz
 export SAMPLES_FN=/data/plasmodium/pfalciparum/pf_community/data/5_0/meta/evaluation_samples_560.txt
 export CALLDATA_FILE_SUFFIX=.calldata_560samples.npy
 
