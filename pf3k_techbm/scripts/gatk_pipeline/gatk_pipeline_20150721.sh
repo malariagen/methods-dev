6888380c87639556eb9cee2985924f43b0bc28e1
Richard Pearson <rpearson@well.ox.ac.uk>
2015-07-22 08:10:45 +0100

    minor

--- pf3k_techbm/scripts/gatk_pipeline/gatk_pipeline_20150721.sh
+++ pf3k_techbm/scripts/gatk_pipeline/gatk_pipeline_20150721.sh
@@ -143,7 +143,7 @@ do
     bwa_mem_fn="${PROCESSED_DATA_DIR}/bams/bwa_mem/${sample_name}.bwa_mem.sam"
     read_group_info=`get_RG "${original_bam_fn}"`
     echo ${read_group_info}
-    echo ${sample_name} ${original_bam_fn} ${bwa_mem_fn}
+    # echo ${sample_name} ${original_bam_fn} ${bwa_mem_fn}
     # if [ ! -f ${bwa_mem_fn} ]; then
     #     if [[ "${original_bam_fn}" == *.bam ]]; then
     #         ${SAMTOOLS_EXE} bamshuf -uOn 128 "${original_bam_fn}" tmp | \
