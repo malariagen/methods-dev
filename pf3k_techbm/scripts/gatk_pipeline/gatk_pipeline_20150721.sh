c137acf22b601afad586c02abf9869be7f883b88
Richard Pearson <rpearson@well.ox.ac.uk>
2015-07-22 08:09:07 +0100

    minor

--- pf3k_techbm/scripts/gatk_pipeline/gatk_pipeline_20150721.sh
+++ pf3k_techbm/scripts/gatk_pipeline/gatk_pipeline_20150721.sh
@@ -144,18 +144,18 @@ do
     read_group_info=`get_RG "${original_bam_fn}"`
     echo ${read_group_info}
     echo ${sample_name} ${original_bam_fn} ${bwa_mem_fn}
-    if [ ! -f ${bwa_mem_fn} ]; then
-        if [[ "${original_bam_fn}" == *.bam ]]; then
-            ${SAMTOOLS_EXE} bamshuf -uOn 128 "${original_bam_fn}" tmp | \
-            ${SAMTOOLS_EXE} bam2fq - | \
-            ${BWA_EXE} mem -M -R '${read_group_info}' -p ${REF_GENOME} - > ${bwa_mem_fn} 2> /dev/null
-        elif [[ "${original_bam_fn}" == *.cram ]]; then
-            ${SAMTOOLS_EXE} view -b "${original_bam_fn}" | \
-            ${SAMTOOLS_EXE} bamshuf -uOn 128 - tmp | \
-            ${SAMTOOLS_EXE} bam2fq - | \
-            ${FIRST_LAST_100BP_EXE} - | \
-            ${BWA_EXE} mem -M -R '${read_group_info}' -p ${REF_GENOME} - > ${bwa_mem_fn} 2> /dev/null
-        fi
-    fi
+    # if [ ! -f ${bwa_mem_fn} ]; then
+    #     if [[ "${original_bam_fn}" == *.bam ]]; then
+    #         ${SAMTOOLS_EXE} bamshuf -uOn 128 "${original_bam_fn}" tmp | \
+    #         ${SAMTOOLS_EXE} bam2fq - | \
+    #         ${BWA_EXE} mem -M -R '${read_group_info}' -p ${REF_GENOME} - > ${bwa_mem_fn} 2> /dev/null
+    #     elif [[ "${original_bam_fn}" == *.cram ]]; then
+    #         ${SAMTOOLS_EXE} view -b "${original_bam_fn}" | \
+    #         ${SAMTOOLS_EXE} bamshuf -uOn 128 - tmp | \
+    #         ${SAMTOOLS_EXE} bam2fq - | \
+    #         ${FIRST_LAST_100BP_EXE} - | \
+    #         ${BWA_EXE} mem -M -R '${read_group_info}' -p ${REF_GENOME} - > ${bwa_mem_fn} 2> /dev/null
+    #     fi
+    # fi
     
 done
