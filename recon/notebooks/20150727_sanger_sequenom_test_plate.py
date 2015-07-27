53fcaaa9d58ce370e14d33e3333de1eefe0ac11b
Richard Pearson <rpearson@well.ox.ac.uk>
2015-07-27 10:24:18 +0100

    New format for sequenom file

---
+++ recon/notebooks/20150727_sanger_sequenom_test_plate.py
@@ -0,0 +1,237 @@
+# -*- coding: utf-8 -*-
+# <nbformat>3.0</nbformat>
+
+# <headingcell level=1>
+
+# Setup
+
+# <codecell>
+
+%run _shared_setup.ipynb
+
+# <codecell>
+
+sanger_sequenom_fn = '/nfs/team112_internal/rp7/recon/sanger_sequenom/processed_data/DK_KR_iPLEX_DK1079_W1381_heights.xls'
+
+# <codecell>
+
+# import brewer2mpl
+# set1 = brewer2mpl.get_map('Set1', 'qualitative', 8).mpl_colors
+genotype_colours = ['lightgray', 'black', 'red', 'blue', 'green']
+
+# <headingcell level=1>
+
+# Functions
+
+# <codecell>
+
+def determine_GT(AD):
+    if sum(AD) < 5:
+        return(None)
+    if len(AD) == 3:
+        if AD[0] > 1 and AD[1] > 1:
+            return("0/1")
+        if AD[0] > 1 and AD[2] > 1:
+            return("0/2")
+        if AD[1] > 1 and AD[2] > 1:
+            return("1/2")
+    if(AD[0] > 1 and AD[1] > 1):
+        return("0/1")
+    if(AD[0] <= 1 and AD[1] > 1):
+        return("1/1")
+    if(AD[0] > 1 and AD[1] <= 1):
+        return("0/0")
+    else:
+        return("unknown")
+    
+
+# <headingcell level=1>
+
+# Load data
+
+# <codecell>
+
+tbl_sanger_sequenom = etl.fromtsv(sanger_sequenom_fn)
+tbl_sanger_sequenom
+
+# <headingcell level=1>
+
+# Scatter plots
+
+# <codecell>
+
+fig = figure(figsize=(12, 7.5))
+for i, ASSAY_ID in enumerate(tbl_sanger_sequenom
+    .distinct('ASSAY_ID')
+    .values('ASSAY_ID')
+    .array()
+):
+    print(ASSAY_ID)
+
+    alleles = (tbl_sanger_sequenom
+        .selecteq('ASSAY_ID', ASSAY_ID)
+        .distinct('ALLELE')
+        .values('ALLELE')
+        .array()
+    )
+
+    # genotypes = [alleles[0], alleles[1], alleles[0]+alleles[1], '']
+    genotypes = (tbl_sanger_sequenom
+        .selecteq('ASSAY_ID', ASSAY_ID)
+        .distinct('GENOTYPE_ID')
+        .addfield('genotype_length', lambda rec: len(rec['GENOTYPE_ID']))
+        .sort('genotype_length')
+        .values('GENOTYPE_ID')
+        .array()
+    )
+
+    ax = fig.add_subplot(3, 5, i+1)
+    for j, genotype in enumerate(insert(genotypes, 0, None)):
+        x_heights = (tbl_sanger_sequenom
+            .selecteq('ASSAY_ID', ASSAY_ID)
+            .selecteq('GENOTYPE_ID', genotype)
+            .selecteq('ALLELE', alleles[0])
+            .values('HEIGHT')
+            .array()
+        )
+        y_heights = (tbl_sanger_sequenom
+            .selecteq('ASSAY_ID', ASSAY_ID)
+            .selecteq('GENOTYPE_ID', genotype)
+            .selecteq('ALLELE', alleles[1])
+            .values('HEIGHT')
+            .array()
+        )
+        ax.scatter(x_heights, y_heights, color=genotype_colours[j], alpha=0.5, label=genotype)
+        assay_split=ASSAY_ID.split('_')
+        assay_title = ASSAY_ID if not ASSAY_ID.startswith('Pf') else "%s %s" % (
+            '_'.join((assay_split[3], assay_split[4], assay_split[5])),
+            assay_split[6]
+        )
+        ax.set_title(assay_title)
+
+fig.tight_layout()
+
+# <codecell>
+
+(tbl_sanger_sequenom
+.convertnumbers()
+.selecteq('ASSAY_ID', 'gb_CM000450_1697797')
+.selectgt('HEIGHT', 50)
+).displayall()
+
+# <codecell>
+
+(tbl_sanger_sequenom
+.convertnumbers()
+.selecteq('ASSAY_ID', 'gb_CM000452_238200')
+.selectgt('HEIGHT', 50)
+).displayall()
+
+# <codecell>
+
+(etl.fromvcf(PGV4_VCF_FN, chrom='Pf3D7_13_v3', start=1725259, stop=1725259)
+    .vcfmeltsamples()
+    .vcfunpackcall()
+    .cutout('GT')
+    .addfield('GT', lambda rec: determine_GT(rec['AD']))
+    .valuecounts('GT')
+)
+
+# <codecell>
+
+
+# <codecell>
+
+fig = figure(figsize=(12, 5))
+for i, ASSAY_ID in enumerate(tbl_sanger_sequenom
+    .distinct('ASSAY_ID')
+    .select(lambda rec: rec['ASSAY_ID'].startswith('Pf'))
+    .values('ASSAY_ID')
+    .array()
+):
+    assay_split=ASSAY_ID.split('_')
+    if(assay_split[0]=='Pf'):
+        print(ASSAY_ID)
+        assay_chrom = '_'.join((assay_split[3], assay_split[4], assay_split[5]))
+        assay_pos = int(assay_split[6])
+
+        alleles = (tbl_sanger_sequenom
+            .selecteq('ASSAY_ID', ASSAY_ID)
+            .distinct('ALLELE')
+            .values('ALLELE')
+            .array()
+        )
+
+        # genotypes = [alleles[0], alleles[1], alleles[0]+alleles[1], '']
+        genotypes = (tbl_sanger_sequenom
+            .selecteq('ASSAY_ID', ASSAY_ID)
+            .distinct('GENOTYPE_ID')
+            .addfield('genotype_length', lambda rec: len(rec['GENOTYPE_ID']))
+            .sort('genotype_length')
+            .values('GENOTYPE_ID')
+            .array()
+        )
+
+        tbl_pgv4 = (etl.fromvcf(PGV4_VCF_FN, chrom=assay_chrom, start=assay_pos, stop=assay_pos)
+            .vcfmeltsamples()
+            .vcfunpackcall()
+            .cutout('GT')
+            .addfield('GT', lambda rec: determine_GT(rec['AD']))
+            .cut(['SAMPLE', 'GT'])
+        )
+
+        ax = fig.add_subplot(2, 5, i+1)
+        for j, genotype in enumerate([None, 'WATER', '0/0', '1/1', '0/1']):
+            x_heights = (tbl_sanger_sequenom
+                .selecteq('ASSAY_ID', ASSAY_ID)
+                .selecteq('ALLELE', alleles[0])
+                .leftjoin(tbl_pgv4, lkey='SAMPLE_ID', rkey='SAMPLE')
+                .update('GT', 'WATER', where=lambda rec: rec['SAMPLE_ID'] == 'WATER')
+                .selecteq('GT', genotype)
+                .values('HEIGHT')
+                .array()
+            )
+            y_heights = (tbl_sanger_sequenom
+                .selecteq('ASSAY_ID', ASSAY_ID)
+                .selecteq('ALLELE', alleles[1])
+                .leftjoin(tbl_pgv4, lkey='SAMPLE_ID', rkey='SAMPLE')
+                .update('GT', 'WATER', where=lambda rec: rec['SAMPLE_ID'] == 'WATER')
+                .selecteq('GT', genotype)
+                .values('HEIGHT')
+                .array()
+            )
+#             y_heights = (tbl_sanger_sequenom
+#                 .selecteq('ASSAY_ID', ASSAY_ID)
+#                 .selecteq('GENOTYPE_ID', genotype)
+#                 .selecteq('ALLELE', alleles[1])
+#                 .values('HEIGHT')
+#                 .array()
+#             )
+            ax.scatter(x_heights, y_heights, color=genotype_colours[j], alpha=0.5, label=genotype)
+            ax.set_title("%s %d" % (assay_chrom, assay_pos))
+
+fig.tight_layout()
+
+# <codecell>
+
+(tbl_sanger_sequenom
+    .selecteq('ASSAY_ID', ASSAY_ID)
+    .selecteq('ALLELE', alleles[0])
+    .leftjoin(tbl_pgv4, lkey='SAMPLE_ID', rkey='SAMPLE')
+    .update('GT', 'WATER', where=lambda rec: rec['SAMPLE_ID'] == 'WATER')
+    .update('GT', 'LAB', where=lambda rec: rec['SAMPLE_ID'].startswith('WT'))
+    .valuecounts('GT')
+    .displayall()
+)
+
+# <codecell>
+
+150+125+70+18+16+5
+
+# <codecell>
+
+print("Number of samples in PGV4 = %d" % (150+70+5))
+
+# <codecell>
+
+
---
+++ recon/scripts/sanger/extract_irods_20150727.py
@@ -0,0 +1,51 @@
+import petl as etl
+import subprocess
+import os
+
+etl.repr_index_header = True
+
+plate_name = "W36000_QC409376_409377_409378_409379_20150715"
+oxford_plate_name = "DK_KR_iPLEX_DK1079_W1381"
+imeta_cmd = "imeta qu -z seq -d sequenom_plate = %s" % plate_name
+irods_data_dir = "/nfs/team112_internal/rp7/recon/sanger_sequenom/irods_data/%s" % plate_name
+combined_data_fn = "/nfs/team112_internal/rp7/recon/sanger_sequenom/combined_data/%s.txt" % plate_name
+processed_data_fn = "/nfs/team112_internal/rp7/recon/sanger_sequenom/processed_data/%s_heights.xls" % oxford_plate_name
+sample_mappings_fn = "/nfs/team112_internal/rp7/recon/sanger_sequenom/sample_mappings/DK1079_sanger_ids_oxford_codes.xlsx"
+rextract_irods = False
+
+subprocess.call("mkdir -p %s" % irods_data_dir, shell=True)
+
+p = subprocess.Popen(imeta_cmd, shell=True, stdout=subprocess.PIPE)
+imeta_out = p.stdout.read().decode("utf-8").split("\n")
+for i in range(int(len(imeta_out)/3)):
+    print(".", end="")
+    irods_dir = imeta_out[i*3].replace("collection: ", "")
+    irods_name = imeta_out[(i*3)+1].replace("dataObj: ", "")
+    irods_path = "%s/%s" % (irods_dir, irods_name)
+    local_path = "%s/%s" % (irods_data_dir, irods_name)
+    if not os.path.exists(local_path) or rextract_irods:
+        icp_cmd = "iget -f %s %s" % (irods_path, local_path)
+        _ = subprocess.call(icp_cmd, shell=True)
+    if i == 0:
+        combined_file_cmd = "cat %s > %s" % (local_path, combined_data_fn)
+    else:
+        combined_file_cmd = "tail -n +2 %s >> %s" % (local_path, combined_data_fn)
+    _ = subprocess.call(combined_file_cmd, shell=True)
+
+tbl_sample_mappings = etl.fromxlsx(sample_mappings_fn, data_only=True)
+
+tbl_processed = (etl.fromtsv(combined_data_fn)
+    .convertnumbers()
+    .update('PLATE', oxford_plate_name)
+    .convert('ASSAY_ID', 'replace', 'W36000-', '')
+    .leftjoin(tbl_sample_mappings, lkey='SAMPLE_ID', rkey='SANGER SAMPLE ID')
+    .cutout('SAMPLE_ID')
+    .rename('sample_code', 'SAMPLE_ID')
+    .addfield('DESCRIPTION', '')
+    .cut(['CUSTOMER', 'PROJECT', 'PLATE', 'EXPERIMENT', 'CHIP', 'WELL_POSITION',
+    'ASSAY_ID', 'GENOTYPE_ID', 'DESCRIPTION', 'SAMPLE_ID', 'ALLELE', 'MASS',
+    'HEIGHT'])
+    .sort(['PLATE', 'WELL_POSITION', 'ASSAY_ID', 'MASS'])
+)
+
+tbl_processed.totsv(processed_data_fn)
